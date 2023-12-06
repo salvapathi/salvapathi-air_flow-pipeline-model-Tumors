172d52a503907153781693ad5d28dcf5cb05b577
YM Srinivasa Salvapathi Naidu <salvapathinaidu@gmail.com>
2023-12-06 17:44:47 +0530

    the first commit Rf model

--- dags/complete_ml_pipeline.py
+++ dags/complete_ml_pipeline.py
@@ -1,4 +1,4 @@
-#Training the Model  and data pipeline
+#Training the Model  and data pipelines
 import airflow
 from airflow import DAG
 from airflow.operators.python_operator import PythonOperator
